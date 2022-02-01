1b4d91584b3db23366a0534e9c8c7aad60dad090
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-02-01 12:09:04 +0100

    decoratorsHTML added

---
+++ slotsHTML/wrappedComponent.js
@@ -0,0 +1,38 @@
+
+class WrappedComponent extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+        let template = document.createElement('template');
+        template.innerHTML = this.getWrappedContent(this.getText());
+        let content = template.content.cloneNode(true);
+        // console.log(content)
+        // let content = this.getWrappedContent(this.getText());
+        this.shadowRoot.appendChild(content);
+    }
+    getWrappedContent(text){
+        return `
+        <style>
+            .wrapped-wrapper{
+                position:relative;
+                width:200px;
+                padding: 10px;
+                background-color:#999;
+                color:white;
+            }
+        </style>
+        <div class="wrapped-wrapper">
+            ${text}
+        </div>
+        `
+    }
+    getText(nrOfRepetitions = 100){
+        let output = ``;
+        for (let i = 0; i < nrOfRepetitions; i++){
+            output += 'Lorem ipsum'
+        }
+        return output;
+    }
+}
+
+window.customElements.define('wrapped-element', WrappedComponent);
